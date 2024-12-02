9ff7827938c6410ab20e4fff26cae9975b286848
themother <jacktowleretc@hotmail.com>
2024-12-02 05:27:08 +0000

    day 2 done

--- day1.py
+++ day1.py
@@ -7,13 +7,13 @@ list0 = [int(i[0]) for i in s]
 list1 = [int(i[1]) for i in s]
 
 
-def day1(l0, l1):
+def part1(l0, l1):
     return reduce(lambda x, y: x + abs(y[0] - y[1]), zip(sorted(l0), sorted(l1)), 0)
 
 
-def day2(l0, l1):
+def part2(l0, l1):
     return reduce(lambda x, y: x + l1.count(y) * y, l0, 0)
 
 
-print(day1(list0, list1))  # 1651298
-print(day2(list0, list1))  # 21306195
+print(part1(list0, list1))  # 1651298
+print(part2(list0, list1))  # 21306195
